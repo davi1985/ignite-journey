410c4942946ff7a7ef7fe9957fe3fb550b6b2d8e
Davi Silva <davisilvaphoto@gmail.com>
2021-05-12 17:56:12 -0300

    02 - add global styles

--- 02-first-web-app-with-react/src/App.tsx
+++ 02-first-web-app-with-react/src/App.tsx
@@ -1,21 +1,11 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-  font-weight: normal;
-
-  span {
-    font-weight: bold;
-  }
-`
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <div className="App">
-      <Title className="title">
-        Hello <span>World</span>
-      </Title>
+      <h1>Hello World</h1>
+
+      <GlobalStyle />
     </div>
   )
 }
---
+++ 02-first-web-app-with-react/src/styles/global.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --backgrund: #f0f2f5;
+    --red:#e52e4d;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --shape: #ffffff;
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; //  14px
+    }
+  }
+
+  body {
+    background: var(--backgrund);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+`

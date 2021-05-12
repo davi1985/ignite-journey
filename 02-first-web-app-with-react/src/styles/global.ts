f5452b76352e0fafca8232c9b9ff3e463d9453e9
Davi Silva <davisilvaphoto@gmail.com>
2021-05-12 18:28:27 -0300

    02 - add google fonts

--- 02-first-web-app-with-react/src/styles/global.ts
+++ 02-first-web-app-with-react/src/styles/global.ts
@@ -2,13 +2,16 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --backgrund: #f0f2f5;
     --red:#e52e4d;
+
     --blue: #5429cc;
     --blue-light: #6933ff;
+    
     --text-title: #363f5f;
     --text-body: #969cb3;
+    
     --shape: #ffffff;
+    --backgrund: #f0f2f5;
   }
   
   * {
@@ -30,6 +33,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(--backgrund);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+     h1, h2, h3, h4, h5, h6, strong {
+    font-weight:600;
   }
 
   button {

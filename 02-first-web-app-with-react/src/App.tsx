99d4a66a0ecabedcdb65d91209713c4031245cfe
Davi Silva <davisilvaphoto@gmail.com>
2021-05-12 17:41:20 -0300

    02 - add styled components

--- 02-first-web-app-with-react/src/App.tsx
+++ 02-first-web-app-with-react/src/App.tsx
@@ -1,3 +1,21 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+  font-weight: normal;
+
+  span {
+    font-weight: bold;
+  }
+`
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="App">
+      <Title className="title">
+        Hello <span>World</span>
+      </Title>
+    </div>
+  )
 }

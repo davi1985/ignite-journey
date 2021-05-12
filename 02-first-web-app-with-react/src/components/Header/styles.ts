c0a5d048b22c33d236b200934e2145f86e9af21e
Davi Silva <davisilvaphoto@gmail.com>
2021-05-12 18:49:09 -0300

    02 - Header component created

---
+++ 02-first-web-app-with-react/src/components/Header/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  background: var(--blue);
+`
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+
+  padding: 2rem 1rem 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    font-size: 1rem;
+    color: #fff;
+    background: var(--blue-light);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    transition: filter 0.3s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+
+  @media (max-width: 720px) {
+    flex-direction: column;
+    padding-bottom: 10rem;
+
+    button {
+      margin-top: 2rem;
+    }
+  }
+`

daf69539b74d8be1ed08fbdd0a8d7de7039f6bd0
Davi Silva <davisilvaphoto@gmail.com>
2021-05-18 08:11:54 -0300

    02 - Created NewTransactionModal

--- 02-first-web-app-with-react/src/App.tsx
+++ 02-first-web-app-with-react/src/App.tsx
@@ -5,6 +5,7 @@ import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
 
 import { GlobalStyle } from './styles/global'
+import { NewTransactionModal } from './components/NewTransactionModal'
 
 Modal.setAppElement('#root')
 export function App() {
@@ -23,12 +24,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>

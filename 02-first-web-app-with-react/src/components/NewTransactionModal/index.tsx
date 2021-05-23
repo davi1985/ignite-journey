6a4491915aaea02aff66eed07e3f1a00c6dcdd2f
Davi Silva <davisilvaphoto@gmail.com>
2021-05-23 10:35:37 -0300

    02 - types button in modal working

--- 02-first-web-app-with-react/src/components/NewTransactionModal/index.tsx
+++ 02-first-web-app-with-react/src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
 import Modal from 'react-modal'
-import { Cointainer, TransactionTypeContainer } from './styles'
+import { Cointainer, TransactionTypeContainer, RadioBox } from './styles'
 
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react'
 
 interface NewTransactionModalProps {
   isOpen: boolean
@@ -14,6 +15,8 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit')
+
   return (
     <Modal
       isOpen={isOpen}
@@ -36,14 +39,23 @@ export function NewTransactionModal({
         <input type="number" placeholder="Valor" />
 
         <TransactionTypeContainer>
-          <button type="button">
+          <RadioBox
+            type="button"
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
+          >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
-          <button type="button">
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => setType('withdraw')}
+            isActive={type === 'withdraw'}
+          >
             <img src={outcomeImg} alt="Saida" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
 
         <input type="text" placeholder="Categoria" />

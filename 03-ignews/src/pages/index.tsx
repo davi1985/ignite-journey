212e7878b61a3e62edca73226cae681830e4fb87
Davi Silva <davisilvaphoto@gmail.com>
2021-10-13 19:17:02 -0300

    03 - add external font

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,12 +1,19 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 import styles from "../styles/home.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <h1 className={styles.title}>
-      Hello <span>World</span>
-    </h1>
+    <>
+      <Head>
+        <title>Home - ig.news</title>
+      </Head>
+
+      <h1 className={styles.title}>
+        Hello <span>World</span>
+      </h1>
+    </>
   );
 };
 

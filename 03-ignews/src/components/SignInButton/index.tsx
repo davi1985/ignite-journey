cd5bd64c0d082bd7337e5b0c6f312486a6d006f8
Davi Silva <davisilvaphoto@gmail.com>
2021-11-01 15:29:24 -0300

    03 - add FaunaDB

--- 03-ignews/src/components/SignInButton/index.tsx
+++ 03-ignews/src/components/SignInButton/index.tsx
@@ -1,13 +1,10 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import { signIn, signOut, useSession } from "next-auth/client";
-
 import styles from "./styles.module.scss";
 
 export const SignInButton = () => {
   const [session] = useSession();
-
-  console.log(session);
 
   return session ? (
     <button type="button" className={styles.signInButton}>

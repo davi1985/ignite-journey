fe4ca3b7d7dd71346345a2299934c11d2835db6b
Davi Silva <davisilvaphoto@gmail.com>
2021-11-19 18:18:21 -0300

    03 - add @testing-library/user-event

--- 03-ignews/src/components/SubscribeButton/SubscribeButton.test.tsx
+++ 03-ignews/src/components/SubscribeButton/SubscribeButton.test.tsx
@@ -1,12 +1,26 @@
-import { render, screen } from "@testing-library/react";
-import { SubscribeButton } from ".";
+import { render, screen } from '@testing-library/react';
+import useEvent from '@testing-library/user-event';
 
-describe("SubscribeButton Component", () => {
-  it("should be render correctly", () => {
+import { SubscribeButton } from '.';
+
+const handleSubscribeMock = jest.fn();
+
+describe('SubscribeButton Component', () => {
+  it('should be render correctly', () => {
     render(<SubscribeButton />);
 
-    const button = screen.getByRole("button", { name: /subscribe now/i });
+    const button = screen.getByRole('button', { name: /subscribe now/i });
 
     expect(button).toBeInTheDocument();
+  });
+
+  it('should possible to call an function when button is clicled', async () => {
+    render(<SubscribeButton />);
+
+    const button = screen.getByRole('button', { name: /subscribe now/i });
+
+    useEvent.click(button, handleSubscribeMock());
+
+    expect(handleSubscribeMock).toBeCalled();
   });
 });

f32093ec2e6db57536c4fece94d591eebc1c34de
Davi Silva <davisilvaphoto@gmail.com>
2021-12-13 10:57:16 -0300

    request to Prismic API

--- 03-ignews/src/pages/posts/index.tsx
+++ 03-ignews/src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import Prismic from '@prismicio/client';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
 export default function Posts() {
@@ -50,3 +53,21 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'publication')],
+    {
+      fetch: ['title', 'content'],
+      pageSize: 100,
+    },
+  );
+
+  console.log(response);
+
+  return {
+    props: {},
+  };
+};

da0fdd636063aecb4a85616b2fd19312589cfdf7
Davi Silva <davisilvaphoto@gmail.com>
2021-12-24 09:42:07 -0300

    fix warning error about prefetch pages

--- 03-ignews/src/components/Header/index.tsx
+++ 03-ignews/src/components/Header/index.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
             <a>Home</a>
           </ActiveLink>
 
-          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/posts">
             <a>Posts</a>
           </ActiveLink>
         </nav>

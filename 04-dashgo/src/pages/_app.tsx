4befda35b14792cdad49c7a42d379c4c1cfbc0ad
Davi Silva <davisilvaphoto@gmail.com>
2022-01-03 11:57:48 -0300

    04-dashgo: fix styles with prettier

--- 04-dashgo/src/pages/_app.tsx
+++ 04-dashgo/src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../styles/theme";
+import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ChakraProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>DashGo Chakra UI</h1>;
+  return <h1>DashGo Chakra UI</h1>
 }

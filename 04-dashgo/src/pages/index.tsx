c141fc0b15b7c9f747fadb2ff146a101e3a17288
Davi Silva <davisilvaphoto@gmail.com>
2022-01-03 15:29:03 -0300

    04-dashgo: create login page

--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,3 +1,62 @@
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from '@chakra-ui/react'
+
 export default function Home() {
-  return <h1>DashGo Chakra UI</h1>
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
 }

0f1c9ff50cc3a5fae1dd7299c2283ace0a2ac53c
Davi Silva <davisilvaphoto@gmail.com>
2022-01-05 16:49:28 -0300

    04-dashgo: add responsive to create user page

--- 04-dashgo/src/pages/users/create.tsx
+++ 04-dashgo/src/pages/users/create.tsx
@@ -19,7 +19,7 @@ const CreateUser = () => (
     <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
       <Sidebar />
 
-      <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+      <Box flex="1" borderRadius="8" bg="gray.800" p={['6', '8']}>
         <Heading size="lg" fontWeight="normal">
           Criar usuário
         </Heading>
@@ -27,12 +27,12 @@ const CreateUser = () => (
         <Divider my="6" borderColor="gray.700" />
 
         <VStack spacing="8">
-          <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+          <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
             <Input name="name" label="Nome Completo" />
             <Input name="email" label="E-mail" type="email" />
           </SimpleGrid>
 
-          <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+          <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
             <Input name="password" label="Senha" type="password" />
             <Input
               name="password_confirmation"
@@ -43,7 +43,7 @@ const CreateUser = () => (
         </VStack>
 
         <Flex mt="8" justify="flex-end">
-          <HStack spacing="8">
+          <HStack spacing="4">
             <Button colorScheme="whiteAlpha">Cancelar</Button>
             <Button colorScheme="pink">Salvar</Button>
           </HStack>

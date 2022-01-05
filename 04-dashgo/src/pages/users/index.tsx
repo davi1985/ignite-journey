d8ff8663968fba1a8c5bad9fcf9f58e2d93edd10
Davi Silva <davisilvaphoto@gmail.com>
2022-01-05 16:31:18 -0300

    04-dashgo: add responsive to UserList page

--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -12,133 +12,170 @@ import {
   Thead,
   Tr,
   Td,
+  useBreakpointValue,
 } from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 
-const UserList = () => (
-  <Box>
-    <Header />
+const UserList = () => {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
 
-    <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-      <Sidebar />
+  return (
+    <Box>
+      <Header />
 
-      <Box flex="1" borderRadius="8" bg="gray.800" p="8">
-        <Flex mb="8" justify="space-between" align="center">
-          <Heading size="lg" fontWeight="normal">
-            Listagem de usuários
-          </Heading>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
 
-          <Button
-            as="a"
-            size="sm"
-            fontSize="sm"
-            colorScheme="pink"
-            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-          >
-            Criar novo
-          </Button>
-        </Flex>
+        <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Listagem de usuários
+            </Heading>
 
-        <Table colorScheme="whiteAlpha">
-          <Thead>
-            <Tr>
-              <Th px="6" color="gray.300" width="8">
-                <Checkbox colorScheme="pink" />
-              </Th>
-              <Th>Usuário</Th>
-              <Th>Data de cadastro</Th>
-              <Th w="8"></Th>
-            </Tr>
-          </Thead>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
 
-          <Tbody>
-            <Tr>
-              <Td px="6">
-                <Checkbox colorScheme="pink" />
-              </Td>
-              <Td>
-                <Box>
-                  <Text fontWeight="bold">Davi Silva</Text>
-                  <Text fontSize="sm" color="gray.300">
-                    davisilvaphoto@gmail.com
-                  </Text>
-                </Box>
-              </Td>
-              <Td>03 de janeiro de 202</Td>
-              <Td>
-                <Button
-                  as="a"
-                  size="sm"
-                  fontSize="sm"
-                  colorScheme="purple"
-                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                >
-                  Editar
-                </Button>
-              </Td>
-            </Tr>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px={['4', '4', '6']} color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
 
-            <Tr>
-              <Td px="6">
-                <Checkbox colorScheme="pink" />
-              </Td>
-              <Td>
-                <Box>
-                  <Text fontWeight="bold">Davi Silva</Text>
-                  <Text fontSize="sm" color="gray.300">
-                    davisilvaphoto@gmail.com
-                  </Text>
-                </Box>
-              </Td>
-              <Td>03 de janeiro de 202</Td>
-              <Td>
-                <Button
-                  as="a"
-                  size="sm"
-                  fontSize="sm"
-                  colorScheme="purple"
-                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                >
-                  Editar
-                </Button>
-              </Td>
-            </Tr>
+                {isWideVersion && <Th>Data de cadastro</Th>}
 
-            <Tr>
-              <Td px="6">
-                <Checkbox colorScheme="pink" />
-              </Td>
-              <Td>
-                <Box>
-                  <Text fontWeight="bold">Davi Silva</Text>
-                  <Text fontSize="sm" color="gray.300">
-                    davisilvaphoto@gmail.com
-                  </Text>
-                </Box>
-              </Td>
-              <Td>03 de janeiro de 202</Td>
-              <Td>
-                <Button
-                  as="a"
-                  size="sm"
-                  fontSize="sm"
-                  colorScheme="purple"
-                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                >
-                  Editar
-                </Button>
-              </Td>
-            </Tr>
-          </Tbody>
-        </Table>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
 
-        <Pagination />
-      </Box>
-    </Flex>
-  </Box>
-)
+            <Tbody>
+              <Tr>
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Davi Silva</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      davisilvaphoto@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                {isWideVersion && <Td>03 de janeiro de 2022</Td>}
+
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={
+                      <Icon
+                        as={RiPencilLine}
+                        fontSize="16"
+                        marginInlineEnd={isWideVersion ? 0 : -2}
+                      />
+                    }
+                  >
+                    {isWideVersion && 'Editar'}
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Davi Silva</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      davisilvaphoto@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>03 de janeiro de 2022</Td>}
+
+                <Td>
+                  <Button
+                    display="flex"
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={
+                      <Icon
+                        as={RiPencilLine}
+                        fontSize="16"
+                        marginInlineEnd={isWideVersion ? 0 : -2}
+                      />
+                    }
+                  >
+                    {isWideVersion && 'Editar'}
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td px={['4', '4', '6']}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Davi Silva</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      davisilvaphoto@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+
+                {isWideVersion && <Td>03 de janeiro de 2022</Td>}
+
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={
+                      <Icon
+                        as={RiPencilLine}
+                        fontSize="16"
+                        marginInlineEnd={isWideVersion ? 0 : -2}
+                      />
+                    }
+                  >
+                    {isWideVersion && 'Editar'}
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
 
 export default UserList

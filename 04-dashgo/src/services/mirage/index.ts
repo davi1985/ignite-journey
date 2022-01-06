b53a23d8b031bc916ae855a839bee2621f3df1d4
Davi Silva <davisilvaphoto@gmail.com>
2022-01-06 17:02:21 -0300

    04-dashgo: add logic to pagination

--- 04-dashgo/src/services/mirage/index.ts
+++ 04-dashgo/src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Model, Factory } from 'miragejs'
+import { createServer, Model, Factory, Response } from 'miragejs'
 import faker from 'faker'
 
 type User = {
@@ -30,14 +30,29 @@ export const makeServer = () => {
     },
 
     seeds(server) {
-      server.createList('user', 10)
+      server.createList('user', 200)
     },
 
     routes() {
       this.namespace = 'api'
       this.timing = 750
 
-      this.get('/users')
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams
+
+        const total = schema.all('user').length
+
+        const pageStart = (Number(page) - 1) * Number(per_page)
+        const pageEnd = pageStart + Number(per_page)
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd,
+        )
+
+        return new Response(200, { 'x-total-count': String(total) }, { users })
+      })
+
       this.post('/users')
 
       this.namespace = ''

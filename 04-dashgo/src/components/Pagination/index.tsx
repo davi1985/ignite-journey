5126f69462d381548f69f4d4e89f9cf30453b024
Davi Silva <davisilvaphoto@gmail.com>
2022-01-07 09:38:11 -0300

    04-dashgo: add update pagination component

--- 04-dashgo/src/components/Pagination/index.tsx
+++ 04-dashgo/src/components/Pagination/index.tsx
@@ -1,23 +1,90 @@
-import { Box, Stack } from '@chakra-ui/react'
+import { Box, Stack, Text } from '@chakra-ui/react'
 import { PaginationItem } from './PaginationItem'
 
-export const Pagination = () => (
-  <Stack
-    direction={['column', 'row']}
-    mt="8"
-    justify="space-between"
-    align="center"
-    spacing="6"
-  >
-    <Box>
-      <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
-    </Box>
+type PaginationProps = {
+  totalCountOfRegisters: number
+  registersPerPage?: number
+  currentPage?: number
+  onPageChange: (page: number) => void
+}
 
-    <Stack direction="row" spacing="2">
-      <PaginationItem number={1} isCurrent />
-      <PaginationItem number={2} />
-      <PaginationItem number={3} />
-      <PaginationItem number={4} />
+const siblingsCount = 1
+
+const generatePagesArray = (from: number, to: number) =>
+  [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1
+    })
+    .filter((page) => page > 0)
+
+export const Pagination = ({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: PaginationProps) => {
+  const lastPage = Math.ceil(totalCountOfRegisters / registersPerPage)
+
+  const previousPages =
+    currentPage > 1
+      ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+      : []
+
+  const nextPages =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + siblingsCount, lastPage),
+        )
+      : []
+
+  return (
+    <Stack
+      direction={['column', 'row']}
+      mt="8"
+      justify="space-between"
+      align="center"
+      spacing="6"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+
+      <Stack direction="row" spacing="2">
+        {currentPage > 1 + siblingsCount && (
+          <>
+            <PaginationItem number={1} />
+
+            {currentPage > 2 + siblingsCount && (
+              <Text color="gray.300" w="8" textAlign="center">
+                ...
+              </Text>
+            )}
+          </>
+        )}
+
+        {previousPages.length > 0 &&
+          previousPages.map((page) => (
+            <PaginationItem number={page} key={page} />
+          ))}
+
+        <PaginationItem number={currentPage} isCurrent />
+
+        {nextPages.length > 0 &&
+          nextPages.map((page) => <PaginationItem number={page} key={page} />)}
+
+        {currentPage + siblingsCount < lastPage && (
+          <>
+            {currentPage + 1 + siblingsCount < lastPage && (
+              <Text color="gray.300" w="8" textAlign="center">
+                ...
+              </Text>
+            )}
+
+            <PaginationItem number={lastPage} />
+          </>
+        )}
+      </Stack>
     </Stack>
-  </Stack>
-)
+  )
+}
--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -127,7 +127,11 @@ const UserList = () => {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={5}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>

f6dbcee2d015d9d81694d8847928690a3e856c94
Davi Silva <davisilvaphoto@gmail.com>
2022-01-10 18:19:48 -0300

    add axios

--- next-auth/context/AuthContext.tsx
+++ next-auth/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext } from "react";
+import { api } from "../services/api";
 import { AuthContextData, AuthProviderProps, SignInCredentials } from "./types";
 
 export const AuthContext = createContext({} as AuthContextData);
@@ -7,7 +8,13 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   const isAuthenticated = false;
 
   const signIn = async ({ email, password }: SignInCredentials) => {
-    console.log({ email, password });
+    try {
+      const response = await api.post("sessions", { email, password });
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (

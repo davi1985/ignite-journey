446183dcb0b26abd0cc6f1ba48af4913c529efda
Davi Silva <davisilvaphoto@gmail.com>
2022-08-30 07:29:59 -0300

    virtualized and bundle analyzer

--- perfreact/pages/components/SearchResults.tsx
+++ perfreact/pages/components/SearchResults.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import { SearchResultsProps } from "../../@types";
 import { ProductItem } from "./ProductItem";
+import { List, ListRowRenderer } from "react-virtualized";
 
 export function SearchResults({
   results,
@@ -11,19 +12,28 @@ export function SearchResults({
     [results]
   );
 
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => (
+    <div key={key} style={style}>
+      <ProductItem
+        key={key}
+        product={results[index]}
+        onAddToWishlist={onAddToWishlist}
+      />
+    </div>
+  );
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map((product) => {
-        return (
-          <ProductItem
-            product={product}
-            key={product.id}
-            onAddToWishlist={onAddToWishlist}
-          />
-        );
-      })}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
